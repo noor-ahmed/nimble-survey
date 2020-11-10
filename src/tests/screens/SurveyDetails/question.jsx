c7de95e6e996f0adea9c3d620904c0a726486158
Noor Ahmed <biswas.noorahmed@gmail.com>
2020-11-10 11:41:38 +0600

    [task-12] Added rating type question screen

---
+++ src/tests/screens/SurveyDetails/question.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import ThumbRating from 'components/Rating';
+// import WheelSelect from 'components/WheelSelect';
+
+const Questions = () => {
+  const question = 'How fulfilled did you feel during this WFH period?';
+
+  return (
+    <div className="questions">
+      <div className="questions__header">
+        <div className="questions__header-right">
+          <button className="button button--close">x</button>
+        </div>
+      </div>
+
+      <div className="questions__container">
+        <div className="questions__details">
+          <div className="questions__number">1/5</div>
+          <h1 className="questions__title"> { question }</h1>
+          <ThumbRating initialRating={ 3 } ratingCount={ 5 } ratingEmoji="smiley" />
+        </div>
+      </div>
+
+      <div className="questions__footer">
+        <div className="questions__footer-right">
+          <button className="button button--arrow">
+            <span className="questions__arrow-right" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Questions;
