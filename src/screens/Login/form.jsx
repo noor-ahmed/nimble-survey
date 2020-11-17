a49f606fae3800289d44ae81f0409492b8d1d12d
Noor Ahmed <biswas.noorahmed@gmail.com>
2020-11-17 16:48:15 +0600

    [task-10] Modified Login functionalities

--- src/screens/Login/form.jsx
+++ src/screens/Login/form.jsx
@@ -1,33 +1,53 @@
 import React from 'react';
 
-function Form() {
+import { Formik } from  'formik';
+import * as Yup from 'yup';
+
+const Form = ({ onSubmitHandler }) => {
+  const validationSchema = Yup.object({
+    email: Yup.string().email('Invalid email address').required('Required'),
+    password: Yup.string().required('Required'),
+  });
+
   return (
-    <form className="form">
-      <div className="form-group__container">
-        <label for="email" className="form-group__label">
-          Email
-        </label>
-        <input type="text" id='email' name="email" className="form-group__input" />
-      </div>
+    <Formik
+      validationSchema={ validationSchema }
+      onSubmit={ onSubmitHandler }
+      validateOnChange={ false }
+      validateOnBlur={ false }
+      className="row"
+    >
+      { ({ errors, isSubmitting }) => (
+        <form className="form">
+          <div className="form-group__container">
+            <label htmlFor="email" className="form-group__label">
+              Email
+            </label>
+            <input type="text" id='email' name="email" className="form-group__input"/>
+          </div>
+          { errors.email && <p className="validation-error">{ errors.email }</p> }
 
-      <div className="form-group">
-        <label for="password" className="form-group__label">
-          Password
-        </label>
-        <div>
-          <input type="password" id="password" name="password" className="form-group__input" />
-          <a href="/forgot-password" className="form-group__forgot-password">
-            Forgot?
-          </a>
-        </div>
-      </div>
+          <div className="form-group">
+            <label htmlFor="password" className="form-group__label">
+              Password
+            </label>
+            <div>
+              <input type="password" id="password" name="password" className="form-group__input"/>
+              <a href="/forgot-password" className="form-group__forgot-password">
+                Forgot?
+              </a>
+            </div>
+          </div>
+          { errors.password && <p className="validation-error">{ errors.password }</p> }
 
-      <div className="footer">
-        <button type="submit" className="btn submit button--white">
-          Sign in
-        </button>
-      </div>
-    </form>
+          <div className="footer">
+            <button type="submit" className="btn submit button--white" disabled={ isSubmitting }>
+              Sign in
+            </button>
+          </div>
+        </form>
+      ) }
+    </Formik>
   );
 };
 
--- src/screens/Login/index.jsx
+++ src/screens/Login/index.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+
 import Layout from '../../components/basics/layout';
 import Logo from '../../components/basics/logo';
 import BackgroundImage from '../../images/background.png';
 import Form from './form';
+import ApiConnector from './api_connector'
+const { handleSubmit, requestSuccess } = ApiConnector();
 
 export class Login extends React.Component {
   render() {
+    if (requestSuccess) {
+      return <Redirect to={ '/' } />;
+    }
+
     return (
       <Layout backgroundImage={ BackgroundImage }>
         <div className="container login-container">
           <div className="content">
             <Logo label={ 'Sign in to Nimble' } />
-            <Form />
+
+            <Form onSubmitHandler={ handleSubmit } />
           </div>
         </div>
       </Layout>
