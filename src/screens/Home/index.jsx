24923dd2b7ebd6009627cacfa28595591c274712
Noor Ahmed <biswas.noorahmed@gmail.com>
2020-12-20 21:45:17 +0600

    [task-13] Modify surveylist

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 
-import Background from 'components/basics/layout';
-import DayDetail from 'components/home/day_detail';
-import Header from 'components/home/header';
+import SurveyAdapter from 'adapters/surveyAdapter';
+import Loader from 'components/Loader';
+import { AuthContext } from 'contexts/auth';
+import { BackgroundProvider } from 'contexts/background';
+import Details from 'screens/Home/details';
 
-export class Home extends React.Component {
-  render() {
-    return (
-      <Background>
-        <div className="container-home">
-          <Header/>
-          <DayDetail/>
-        </div>
-      </Background>
-    );
-  }
-}
+const Home = () => {
+  const [response, setResponse] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const authContext = useContext(AuthContext);
+
+  useEffect(() => {
+    getList();
+  }, []);
+
+  const getList = async () => {
+    try {
+      await SurveyAdapter.getList(1, authContext.state.authorization_token).then(
+        function (response) {
+          if (response.status === 200) {
+            setResponse(response.data);
+            setIsLoading(false);
+          }
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <BackgroundProvider>
+      { isLoading ? <Loader /> : <Details surveyResponse={ response } /> }
+    </BackgroundProvider>
+  );
+};
 
 export default Home;
