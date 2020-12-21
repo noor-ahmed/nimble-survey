c56dd9658209b2be95fe0cbfc4e3df8708b406ca
Noor Ahmed <biswas.noorahmed@gmail.com>
2020-12-21 06:33:03 +0600

    [task-19] WIP

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,17 +3,19 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Login from 'screens/Login';
-import ForgotPassword from 'screens/ForgotPassword'
-import LazyLoader from 'components/LazyLoader'
-import SurveyDetail from 'screens/Home/surveyList';
+import ForgotPassword from 'screens/ForgotPassword';
+import LazyLoader from 'components/LazyLoader';
+import SurveyList from 'screens/Home/surveyList';
 import Home from 'screens/Home';
+import SurveyDetail from 'screens/SurveyDetail';
 
 const RouteList = () => {
   return (
     <Switch>
       <Route exact path="/" component={ Login }></Route>
       <Route path="/forgot-password" component={ ForgotPassword }></Route>
-      <Route path="/survey/" component={ SurveyDetail } />
+      <Route path="/survey/" component={ SurveyList } />
+      <Route path="/survey/:surveyId" component={ SurveyDetail } />
       <Route path="/loader" component={ LazyLoader }></Route>
       <Route path="/home" component={ Home }></Route>
     </Switch>
